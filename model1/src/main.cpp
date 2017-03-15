751ecea2608173f3173fd0022c64d419e5cf22be
Marcus Edel <marcus.edel@fu-berlin.de>
2017-03-15 17:17:23 +0100

    Initial model idea.

---
+++ model1/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <mlpack/core.hpp>
+
+int main(int argc, char **argv)
+{
+  arma::mat temp = arma::zeros(5, 5);
+
+  std::cout << "project 1\n";
+  return 0;
+}
---
+++ model2/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <mlpack/core.hpp>
+
+int main(int argc, char **argv)
+{
+  arma::mat temp = arma::zeros(5, 5);
+
+  std::cout << "project 2\n";
+  return 0;
+}
